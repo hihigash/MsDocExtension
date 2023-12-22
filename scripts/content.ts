53cd2baa3e3fee6e2de18f7ba8f8f0d0792cd617
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2023-12-23 00:22:01 +0900

    Refactor content.ts to improve button creation and URL handling

--- scripts/content.ts
+++ scripts/content.ts
@@ -1,14 +1,28 @@
-import { mappingUrl } from "./mappingUrl";
-
 const url = new URL(window.location.href);
-console.log(url.href);
 const actions = document.getElementById("article-header-page-actions");
 if (actions) {
+  const button = createButton();
+  if (actions.firstChild) {
+    actions.insertBefore(button, actions.firstChild);
+  } else {
+    actions.appendChild(button);
+  }
+}
+
+function createButton(): HTMLButtonElement {
   const button = document.createElement("button");
-  button.classList.add("collection", "button", "button-clear", "button-sm", "button-primary", "display-none", "display-inline-flex-tablet");
+  button.classList.add(
+    "collection",
+    "button",
+    "button-clear",
+    "button-sm",
+    "button-primary",
+    "display-none",
+    "display-inline-flex-tablet"
+  );
   button.title = "Open Markdown on GitHub";
   button.addEventListener("click", () => {
-    OpenConvrtedUrl(url.href);
+    openConvertedUrl(url.href).catch(console.error);
   });
   const span = document.createElement("span");
   span.classList.add("icon", "margin-none");
@@ -18,25 +32,31 @@ if (actions) {
   icon.classList.add("docon", "docon-brand-github");
   span.appendChild(icon);
 
-  if (actions.firstChild) {
-    actions.insertBefore(button, actions.firstChild);
-  } else {
-    actions.appendChild(button);
+  return button;
+}
+
+async function openConvertedUrl(url: string) {
+  const enUrl = convertUrl(url);
+  const ghUrl = await fetchGithubUrl(enUrl);
+  if (ghUrl) {
+    open(ghUrl);
   }
 }
 
-function OpenConvrtedUrl(url: string) {
-  const enUrl = url.replace(/https:\/\/learn\.microsoft\.com\/[a-z]{2}-[a-z]{2}\//g, 'https://learn.microsoft.com/en-us/');
-  console.log(enUrl);
-  const result = mappingUrl.find((i) => enUrl.startsWith(i.baseUrl));
-  if (result) {
-    console.log(result.baseUrl)
-    const escapedBaseUrl = result.baseUrl.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
-    const regex = new RegExp(`${escapedBaseUrl}(.*)\\?.*|${escapedBaseUrl}(.*)`, 'g');
-    const newUrl = enUrl.replace(regex, `${result.repoUrl}$1$2.md`);
-    console.log(newUrl);
-    window.open(newUrl);
-  } else {
-    window.alert("No matching GitHub repository found for this document.");
-  }
+function convertUrl(url: string): string {
+  return url.replace(
+    /https:\/\/learn\.microsoft\.com\/[a-z]{2}-[a-z]{2}\//g,
+    "https://learn.microsoft.com/en-us/"
+  );
+}
+
+async function fetchGithubUrl(url: string): Promise<string | null> {
+  const response = await fetch(url);
+  if (!response.ok) return null;
+  const text = await response.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text || "", "text/html");
+  const edit = doc.querySelector("a[aria-label='Edit']");
+  const hrefAttr = edit?.attributes.getNamedItem("href");
+  return hrefAttr ? hrefAttr.value : null;
 }
