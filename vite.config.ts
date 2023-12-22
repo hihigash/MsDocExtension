9e935d705496cc82c0d92973f5b80330ebbbb5bf
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2023-12-22 22:20:48 +0900

    workaround the error: vite manifest is missing

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
-import { defineConfig } from 'vite'
-import { crx, defineManifest } from '@crxjs/vite-plugin'
+import { defineConfig } from "vite";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: 'My Extension',
-  description: 'My Extension',
-  version: '0.0.1',
+  name: "MSDOC2GHMD",
+  description:
+    "Open the markdown file on the Github repo from the current Microsoft document.",
+  version: "0.0.1",
   icons: {
-    16: 'icons/16.png',
-    32: 'icons/32.png',
-    48: 'icons/48.png',
-    128: 'icons/128.png',
+    16: "icons/16.png",
+    32: "icons/32.png",
+    48: "icons/48.png",
+    128: "icons/128.png",
   },
   content_scripts: [
     {
-      js: ['scripts/content.ts'],
-      matches: ['https://learn.microsoft.com/*'],
-    }
-  ],});
+      js: ["scripts/content.ts"],
+      matches: ["https://learn.microsoft.com/*"],
+    },
+  ],
+});
 
-  export default defineConfig({
-    plugins: [crx({ manifest})],
-  });
+const viteManifestHackIssue846: Plugin & { renderCrxManifest: (manifest: any, bundle: any) => void } = {
+  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
+  name: 'manifestHackIssue846',
+  renderCrxManifest(_manifest, bundle) {
+      bundle['manifest.json'] = bundle['.vite/manifest.json']
+      bundle['manifest.json'].fileName = 'manifest.json'
+      delete bundle['.vite/manifest.json']
+  },
+}
+
+export default defineConfig({
+  plugins: [viteManifestHackIssue846, crx({ manifest })],
+});
